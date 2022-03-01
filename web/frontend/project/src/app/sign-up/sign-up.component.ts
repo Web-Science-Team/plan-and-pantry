0519163b7eaa6a89ef70f9ea20769b49741088a6
weix3 <43729904+weix3@users.noreply.github.com>
2022-03-01 14:18:34 -0500

    Update sign-up.component.ts

--- web/frontend/project/src/app/sign-up/sign-up.component.ts
+++ web/frontend/project/src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
-
+  loginForm: any;
+  hide: any;
   constructor() { }
 
   ngOnInit(): void {
+    this.loginForm = new FormGroup(
+      {
+        email: new FormControl('', [Validators.required, Validators.email]),
+        password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+        confirmPassword: new FormControl('', [Validators.required])
+      }
+    );
+
+    this.hide = true;
+  }
+  onLogin(){
+
   }
 
+  confirmValidation(): boolean {
+      return this.loginForm.password == this.loginForm.confirmPassword;
+  }
 }
