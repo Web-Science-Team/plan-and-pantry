534bb5c3c50be3846668308b5a6705060887a684
weix3 <43729904+weix3@users.noreply.github.com>
2022-03-03 17:33:49 -0500

    Update app-routing.module.ts

--- web/frontend/project/src/app/app-routing.module.ts
+++ web/frontend/project/src/app/app-routing.module.ts
@@ -3,22 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { ForgetPasswordComponent } from './forget-password/forget-password.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
-  {
-    path : '',
-    component : AppComponent,
-    children : [
-      {
-        path:'',
-        component: RecipesComponent
-      }
-    ]
-
-  }
-
-
-];
+  {path: 'signin-component', component: SignInComponent},
+  {path: 'signup-component', component: SignUpComponent},
+  {path: 'recipes-component', component: RecipesComponent},
+  {path: 'forgetPassword-component', component: ForgetPasswordComponent},
+  {path: 'profile-component', component: ProfileComponent},
+  {path: '', redirectTo: '/signin-component', pathMatch: 'full'}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
