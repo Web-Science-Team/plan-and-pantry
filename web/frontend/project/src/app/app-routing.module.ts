f536716624ef59ae8aba0bb490a98775f631981b
weix3 <43729904+weix3@users.noreply.github.com>
2022-04-22 11:43:32 -0400

    Update app-routing.module.ts

--- web/frontend/project/src/app/app-routing.module.ts
+++ web/frontend/project/src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { SignInComponent } from './sign-in/sign-in.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { HomeComponent } from './home/home.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
 
 const routes: Routes = [
   {path: 'login', component: SignInComponent},
   {path: 'register', component: SignUpComponent},
   {path: 'recipe-search', component: RecipesComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'recipe-list', component: RecipeListComponent}
 ]
 
 @NgModule({
