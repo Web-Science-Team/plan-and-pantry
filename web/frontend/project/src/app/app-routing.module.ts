7f86d468397d7cfc0060b34965d58f2c7a66486b
Patrick Fernandez <fernap4@rpi.edu>
2022-04-23 20:25:10 -0400

    r1 needs more work

--- web/frontend/project/src/app/app-routing.module.ts
+++ web/frontend/project/src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
   {path: 'login', component: SignInComponent},
   {path: 'register', component: SignUpComponent},
   {path: 'recipe-search', component: RecipesComponent},
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '', redirectTo: '/single-view', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'single-view', component: SingleRecipeComponent}
 ]
--- web/frontend/project/src/app/app.module.ts
+++ web/frontend/project/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
-import {MatToolbarModule } from '@angular/material/toolbar'
+import { MatToolbarModule } from '@angular/material/toolbar'
 
 import { MatIconModule } from '@angular/material/icon';
 
@@ -18,6 +18,7 @@ import { ReactiveFormsModule} from '@angular/forms';
 import { RecipesComponent } from './recipes/recipes.component';
 import { HomeComponent } from './home/home.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { SingleRecipeComponent } from './single-recipe/single-recipe.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     SignUpComponent,
     RecipesComponent,
     HomeComponent,
-    SidebarComponent
+    SidebarComponent,
+    SingleRecipeComponent
   ],
   imports: [
     BrowserModule,
