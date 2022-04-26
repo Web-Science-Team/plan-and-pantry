0e9ec429094a824b4be3f6f85bbd3b9253b4e256
callumhauber <callumhauber@gmail.com>
2022-04-25 23:20:23 -0400

    add route guards

--- web/frontend/project/src/app/app-routing.module.ts
+++ web/frontend/project/src/app/app-routing.module.ts
@@ -9,21 +9,22 @@ import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { SingleRecipeComponent } from './single-recipe/single-recipe.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { LoggedInHomeComponent } from './logged-in-home/logged-in-home.component';
+import { AuthGuard } from './guard/auth.guard';
 
 const routes: Routes = [
-  { path: 'login', component: SignInComponent },
-  { path: 'register', component: SignUpComponent },
-  { path: 'recipe-search', component: RecipesComponent },
-  { path: 'recipe-list', component: RecipeListComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'single-view', component: SingleRecipeComponent },
-  { path: 'sidebar', component: SidebarComponent },
-  { path: 'logged-in-home', component: LoggedInHomeComponent }
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'login', component: SignInComponent },
+    { path: 'register', component: SignUpComponent },
+    { path: 'recipe-search', component: RecipesComponent, canActivate: [AuthGuard] },
+    { path: 'recipe-list', component: RecipeListComponent, canActivate: [AuthGuard] },
+    { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: 'single-view', component: SingleRecipeComponent, canActivate: [AuthGuard] },
+    { path: 'sidebar', component: SidebarComponent, canActivate: [AuthGuard] },
+    { path: 'logged-in-home', component: LoggedInHomeComponent, canActivate: [AuthGuard] }
 ]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
