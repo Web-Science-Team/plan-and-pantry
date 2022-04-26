6204e8bab7776a0f2b9802dce408c5bda5c36cbd
callumhauber <callumhauber@gmail.com>
2022-04-25 22:54:07 -0400

    add sign up code

--- web/frontend/project/src/app/services/auth.service.ts
+++ web/frontend/project/src/app/services/auth.service.ts
@@ -49,7 +49,7 @@ export class AuthService {
       .then((result) => {
         /* Call the SendVerificaitonMail() function when new user sign 
         up and returns promise */
-        this.SendVerificationMail();
+        // this.SendVerificationMail();
         this.SetUserData(result.user);
       })
       .catch((error) => {
