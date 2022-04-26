9484c6711b8f18b6adee3a416286fdfae676d71d
callumhauber <callumhauber@gmail.com>
2022-04-26 00:46:11 -0400

    setup concurrently for backend

--- web/backend/server.js
+++ web/backend/server.js
@@ -1,38 +1,23 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const axios = require('axios')
 const path = require('path');
 
 const app = express();
 const port = 3000;
 
-//app.use(express.static(path.join(__dirname, '../frontend/project/dist/project')));
+app.use(express.static(path.join(__dirname, '../frontend/project/dist/project')));
 
-// here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// here we are configuring express
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: false, parameterLimit: 10000 }));
 
-/*
-function makeHeader(allow, link) {
-  return {
-    'allow': allow.join(),
-    'link': link
-  }
-}
-*/
-/*
 app.get('/', (req, res) => {
   res.send();
 });
-*/
-/*
-app.use(express.static(path.join(__dirname, '../frontend/project/dist/project')));
-app.use('/', express.static('../frontend/project/dist/project'))
-*/
-app.use(express.static("../frontend/project/dist/project"));
-app.use((req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/project/dist/project/index.html'));
-});
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, "../frontend/project/dist/project/index.html"));
+})
 
 app.listen(port, () => {
   console.log(`Listening on *:${port}`);
